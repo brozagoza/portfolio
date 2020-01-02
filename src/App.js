37348ed005533ff383235772df878fefbecec059
brozagoza <xelazgoza@gmail.com>
2020-01-02 15:56:32 -0800

    stuff

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Zgoza's dank site is here.
         </p>
         <a
           className="App-link"
